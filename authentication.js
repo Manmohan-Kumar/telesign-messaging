8029762b1da64ff6b3d2ebba916aacddb7b94bbd
Manmohan <manmohan.kumar@techmahindra.com>
2018-08-17 18:33:43 +0530

    Updated Auth Test to include phoneID

--- authentication.js
+++ authentication.js
@@ -1,7 +1,6 @@
 const testTrigger = require('./triggers/init');
 
-const authentication = {
-  // TODO: just an example stub - you'll need to complete
+const authentication = {  
   type: 'custom',
 
   test: testTrigger.operation.perform,
@@ -24,7 +23,7 @@ const authentication = {
       key: 'test_phone_number',
       label: 'Test Phone Number' ,      
       type: 'string',
-      helpText: 'Provide your phone number with the international country code to receive a text to verify your credentials. A text will be sent to the number if successful. E.g. +12141112222',
+      helpText: 'Provide your phone number with the international country code to verify your credentials. E.g. +12141112222',
       required: true
     },
     {
--- triggers/init.js
+++ triggers/init.js
@@ -3,13 +3,12 @@ const { replaceVars } = require('../utils');
 const getList = (z, bundle) => {  
   baseURL = bundle.authData.baseURL?bundle.authData.baseURL:'https://rest-api.telesign.com';
 
-  let authMsgTestURL = '/v1/messaging';
-  url = baseURL + authMsgTestURL;
+  let authTestURL = '/v1/phoneid/' + bundle.authData.test_phone_number;
+  url = baseURL + authTestURL;
   url = replaceVars(url, bundle);
   const responsePromise = z.request({ 
     url: url,
-    method: 'POST',
-    body: 'phone_number=' + bundle.authData.test_phone_number+'&message=Zapier welcomes you to TeleSign'+'&message_type=ARN',
+    method: 'POST'
    });
   return responsePromise.then(response => {
     let errorMessage = 'The credentials you provided are invalid.';
@@ -67,13 +66,116 @@ module.exports = {
         default: '917003400580'
       }
     ],
-    outputFields: [
+    outputFields: [      
       {
-        key: 'additional_info__message_parts_count',
+        key: 'reference_id',
+        type: 'string'
+      },
+      { key: 'phone_type__code',
+        type: 'string'
+      },
+      { key: 'phone_type__description',
+        type: 'string'
+      },
+      { key: 'numbering__original__phone_number',
+        type: 'string'
+      },
+      { key: 'numbering__original__complete_phone_number',
+        type: 'string'
+      },
+      { key: 'numbering__original__country_code',
         type: 'string'
       },
       {
-        key: 'reference_id',
+        key: 'numbering__cleansing__call__cleansed_code',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__call__country_code',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__call__max_length',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__call__min_length',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__call__phone_number',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__sms__cleansed_code',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__sms__country_code',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__sms__max_length',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__sms__min_length',
+        type: 'string'
+      },
+      {
+        key: 'numbering__cleansing__sms__phone_number',
+        type: 'string'
+      },
+      {
+        key: 'location__city',
+        type: 'string'
+      },
+      {
+        key: 'location__county',
+        type: 'string'
+      },
+      {
+        key: 'location__state',
+        type: 'string'
+      },
+      {
+        key: 'location__zip',
+        type: 'string'
+      },
+      {
+        key: 'country__iso2',
+        type: 'string'
+      },
+      {
+        key: 'country__iso3',
+        type: 'string'
+      },
+      {
+        key: 'country__name',
+        type: 'string'
+      },
+      {
+        key: 'timezone__name',
+        type: 'string'
+      },
+      {
+        key: 'timezone__utc_offset_max',
+        type: 'string'
+      },
+      {
+        key: 'timezone__utc_offset_min',
+        type: 'string'
+      },
+      {
+        key: 'coordinates__latitude',
+        type: 'string'
+      },
+      {
+        key: 'coordinates__longitude',
+        type: 'string'
+      },
+      {
+        key: 'metro_code',
         type: 'string'
       },
       {
@@ -90,9 +192,17 @@ module.exports = {
       }
     ],
     perform: getList,
-    sample: {
-      additional_info: { message_parts_count: 1 },
+    sample: {      
       reference_id: '0123456789ABCDEF0123456789ABCDEF',
+      phone_type:{code: '3', description: 'PREPAID'},
+      numbering: {original:{phone_number:'6026021234', complete_phone_number:'6026021234', country_code:'1'}, 
+                cleansing:{call:{cleansed_code:'100',country_code:'1', max_length:'1', min_length:'1',phone_number:'6026021234'},
+                           sms:{cleansed_code:'100',country_code:'1', max_length:'1', min_length:'1',phone_number:'6026021234'}}},
+      location: {city:'New York', county:'New York County', state:'New York', zip:'10001'},
+      country: {iso2:'US', iso3:'USA', name:'United States'},
+      timezone:{name:'America/Los_Angeles', utc_offset_max:'-08:00', utc_offset_min: '-08:00'},
+      coordinates:{latitude:'34.18264', longitude:'-118.30840'},
+      metro_code:'4480',
       status: { code: 290, description: 'Message in progress', updated_on: '2015-10-03T14:51:28.709526Z' }
     }
   }
